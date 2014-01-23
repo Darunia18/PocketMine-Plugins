c544fad3c719a7708e7c027dde43876737f287d4
Darunia18 <darunia1818@gmail.com>
2014-01-22 21:25:55 -0500

    Added $ on 29

--- EnderChest.php
+++ EnderChest.php
@@ -26,7 +26,7 @@ class EnderChest implements Plugin {
 		
 	public function touchHandler($data){
 		$username = $data["player"]->username;
-		ChestID = $data["target"]->getID(54);
+		$ChestID = $data["target"]->getID(54);
 		if($ChestID = 54){
 			if(file_exists('./plugins/EnderChest/'.$username.'.yml')){
 				$level = $data["target"]->level;//Chest level. I have no clue if this is right.
