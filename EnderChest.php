91fc74df40a6652af129721f20c47c86122270da
Darunia18 <darunia1818@gmail.com>
2014-02-23 00:31:35 -0500

    Lots of fixes

--- EnderChest.php
+++ EnderChest.php
@@ -22,15 +22,15 @@ class EnderChest implements Plugin {
 	}	
 		
 	public function touchHandler($data, $packet){
-		$username = $packet->username;
-		$x = $data["target"]->x;
+		$username = $data["player"]->username;
+		/*$x = $data["target"]->x;
 		$y = $data["target"]->y;
 		$z = $data["target"]->z;
-		$level = $data["target"]->level;
+		$level = $data["target"]->level;*/
 		if($ChestID = 54){
 			if(file_exists('./plugins/EnderChest/'.$username.'.yml')){
-				$this->api->tile->get(new Position($x, $y, $z, $level));
-				$this->config = $this->api->plugin->configPath($this . $username . ".yml");
+				$chest = $this->api->tile->get(new Position($data['target']->x, $data['target']->y, $data['target']->z, $data['target']->level));
+				$this->config = $this->api->plugin->configPath($this).$username.".yml";
 				$chest->setSlot(0,$this->config->get('Slot0'['id']));
 				$chest->setSlot(1,$this->config->get('Slot1'['id']));
 				$chest->setSlot(2,$this->config->get('Slot2'['id']));
